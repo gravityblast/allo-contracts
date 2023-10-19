529a7d076fc949fecff0ac8150c9f6470fe9be80
Andrea Franz <andrea@gravityblast.com>
2023-10-19 11:18:12 +0200

    add deterministic address for the project registry proxy contract

--- scripts/config/projectRegistry.config.ts
+++ scripts/config/projectRegistry.config.ts
@@ -7,7 +7,7 @@ type DeployParams = Record<string, ProjectRegistryParams>;
 
 export const projectRegistryParams: DeployParams = {
   dev: {
-    proxyContractAddress: "",
+    proxyContractAddress: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0",
   },
   mainnet: {
     proxyContractAddress: "",
