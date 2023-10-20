f3357d3d28507ae5d809f63cafb61b5b1eb4767a
Andrea Franz <andrea@gravityblast.com>
2023-10-20 10:04:04 +0200

    create more projects and add a new project for the second account

--- scripts/dev/populate/projects.ts
+++ scripts/dev/populate/projects.ts
@@ -3,28 +3,32 @@ import hre, { ethers } from "hardhat";
 async function main() {
   console.log("🟡 Creating projects");
 
+  const [account1, account2] = await ethers.getSigners();
+
   if (hre.network.name !== "dev") {
     console.error("This script can only be use in local dev environments");
     process.exit(1);
   }
 
-  let account;
-  let accountAddress;
-
-  account = (await ethers.getSigners())[0];
-  accountAddress = await account.getAddress();
-
   console.log("This script populates the local chain");
-
   const projectRegistry = await ethers.getContractAt(
     "ProjectRegistry",
-    "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0",
-    account
+    "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
   );
 
-  await projectRegistry.createProject({
+  await projectRegistry.connect(account1).createProject({
     protocol: 1,
-    pointer: "QmWnDfdggaDkoQQ1r8fSUr6f2yN25ERqAtbYbDtdxTrvUx",
+    pointer: "QmbxFwXhyTBVfxS5zDhKVB6GmdzcQRUNyTHbRNnxQ2iqDB",
+  });
+
+  await projectRegistry.connect(account1).createProject({
+    protocol: 1,
+    pointer: "QmfXWmPeVHFk8UKRQp6gqfV5MDKj25W1AVtpHRi18nDkPG",
+  });
+
+  await projectRegistry.connect(account2).createProject({
+    protocol: 1,
+    pointer: "QmPfuocTLNvBPqrCbTbQ4wV7tigFRkGVjKYj8ai9kPeUga",
   });
 
   console.log("✅ Projects created");
